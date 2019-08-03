3cf85305fdfd0847d9db9b4ffc18c897eb527642
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2019-08-04 01:20:58 +0300

    remove comments

--- ChatApp/ChatApp/ViewModel/DisplayImageViewModel.cs
+++ ChatApp/ChatApp/ViewModel/DisplayImageViewModel.cs
@@ -1,11 +1,8 @@
 ﻿using ChatApp.Command;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace ChatApp.ViewModel
@@ -44,31 +41,14 @@ namespace ChatApp.ViewModel
 
     private void SelectImageCommandExecute()
     {
-
-      //System.Windows.Forms.DialogResult result = dialog.ShowDialog();
       Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
       dialog.Filter = "All files (*.*)|*.*|PNG files (*.png)|*.png*|JPG files (*.jpg)|*.jpg*";
       dialog.ShowDialog();
-
       var newFile = dialog.FileName;
-
-      {
-        Task.Factory.StartNew(() =>
-        {
-          var file = Directory.GetFiles(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "/Resources", "ProfilePicture.*").FirstOrDefault();
-          ProfilePicturePath = "D:\\GitHub\\ChatApp\\ChatApp\\ChatApp\\Resources\\SendImage.png";
-          var fs = new FileStream(file, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
-          File.Delete(file);
-          //writer.WriteLine(content);
-          //File.Replace(newFile, file, file + ".bck");
-          File.Copy(newFile, Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "/Resources/ProfilePicture" + Path.GetExtension(newFile));
-          ProfilePicturePath = newFile;
-        });
-        //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ProfilePicturePath"));
-
-      }
-
-
+      var file = Directory.GetFiles(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "/Resources", "ProfilePicture.*").FirstOrDefault();
+      File.Delete(file);
+      File.Copy(newFile, Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "/Resources/ProfilePicture2" + Path.GetExtension(newFile));
+      ProfilePicturePath = newFile;
     }
   }
 }
