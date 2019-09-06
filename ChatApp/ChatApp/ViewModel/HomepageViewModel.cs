8a6e466ff5e7402b1407df40ea5bcd2d3cc4a1aa
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2019-09-06 15:05:46 +0300

    refactor homePageViewModel

--- ChatApp/ChatApp/ViewModel/HomepageViewModel.cs
+++ ChatApp/ChatApp/ViewModel/HomepageViewModel.cs
@@ -2,7 +2,6 @@
 using ChatApp.Helper;
 using ChatApp.Model;
 using ChatApp.Properties;
-using ChatApp.View;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -10,33 +9,20 @@ using System.ComponentModel;
 using System.Drawing;
 using System.IO;
 using System.Windows;
-using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using HorizontalAlignment = System.Windows.HorizontalAlignment;
-using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace ChatApp.ViewModel
 {
   class HomepageViewModel : INotifyPropertyChanged
   {
-    #region Properties
+    #region Public Properties
+
     public Action CloseAction { get; set; }
-
-    private Window window;
-
     public ICommand LogoutCommand { get; set; }
-
     public ICommand SendCommand { get; set; }
     public string IsActiveImagePath { get; set; } = UserModel.Instance.IsActive ? "pack://application:,,,/ChatApp;component/Resources/IsOnline.png" : "pack://application:,,,/ChatApp;component/Resources/IsOffline.png";
-
-    private string _welcomeText;
-
-    private ObservableCollection<MessageModel> messagesList;
-
-    private ObservableCollection<UserModel> contactsList;
-    private string messageBoxContent;
-    private string _email;
 
     public string Email
     {
@@ -93,20 +79,6 @@ namespace ChatApp.ViewModel
       }
     }
 
-    public ObservableCollection<UserModel> ContactsList
-    {
-      get { return contactsList; }
-      set
-      {
-        if (contactsList == value) return;
-        contactsList = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ContactsList"));
-      }
-    }
-
-    private BitmapImage profilePicturePath;
-    private List<ContactListsModel> _contactsLists;
-
     public BitmapImage ProfilePicturePath
     {
       get { return profilePicturePath; }
@@ -120,20 +92,62 @@ namespace ChatApp.ViewModel
 
     #endregion
 
+    #region Private Properties
+
+    private string _welcomeText;
+    private string _email;
+    private string messageBoxContent;
+    private Window window;
+    private ObservableCollection<MessageModel> messagesList;
+    private BitmapImage profilePicturePath;
+    private List<ContactListsModel> _contactsLists;
+
+    #endregion
+
     #region Constructor
+
     public HomepageViewModel(Window window)
     {
       this.window = window;
-      WelcomeText = "Welcome ";
-      Email = UserModel.Instance.Email;
+
       LogoutCommand = new RelayCommand(LogoutCommandExecute);
       SendCommand = new RelayCommand(SendCommandExecute);
 
-      ContactsLists = new List<ContactListsModel>();
+      InitializeContactList();
+      InitializeUserProfile();
+    }
 
+    private void InitializeUserProfile()
+    {
+      WelcomeText = "Welcome ";
+
+      Email = UserModel.Instance.Email;
+
+      ProfilePicturePath = new BitmapImage(new Uri(@"pack://application:,,,/ChatApp;component/Resources/ProfilePicture.jpg"));
+      ProfilePicturePath.CacheOption = BitmapCacheOption.None;
+      var path = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())) + "\\Resources\\ProfilePicture.jpg";
+      if (!File.Exists(path))
+      {
+        File.Create(path);
+      }
+      else
+      {
+        Image image = Image.FromFile(Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())) + "\\Resources\\ProfilePicture.jpg");
+        image.Dispose();
+      }
+    }
+
+    private void InitializeContactList()
+    {
+      ContactsLists = new List<ContactListsModel>();
+      AddRecentContactsFakeList();
+      AddAdressBookFakeList();
+    }
+
+    private void AddRecentContactsFakeList()
+    {
       var recent = new ContactListsModel
       {
-        ListName = "Recent",
         ImagePath = "pack://application:,,,/ChatApp;component/Resources/RecentContact_Icon.png",
         ContactsList = new List<DisplayedContactModel>()
       };
@@ -144,10 +158,13 @@ namespace ChatApp.ViewModel
       });
 
       recent.ListName = string.Format("Recent Contacts (0/{0})", recent.ContactsList.Count);
+      ContactsLists.Add(recent);
+    }
 
+    private void AddAdressBookFakeList()
+    {
       var addressBook = new ContactListsModel
       {
-        ListName = "Family",
         ContactsList = new List<DisplayedContactModel>()
       };
       addressBook.ContactsList.Add(new DisplayedContactModel
@@ -179,41 +196,7 @@ namespace ChatApp.ViewModel
       });
 
       addressBook.ListName = string.Format("Address Book (0/{0})", addressBook.ContactsList.Count);
-
-      ContactsLists.Add(recent);
       ContactsLists.Add(addressBook);
-
-      ProfilePicturePath = null;
-      ProfilePicturePath = new BitmapImage(
-        new Uri(@"pack://application:,,,/ChatApp;component/Resources/ProfilePicture.jpg"));
-      ProfilePicturePath.CacheOption = BitmapCacheOption.None;
-      var path = Path.GetDirectoryName(
-          Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())) +
-          "\\Resources\\ProfilePicture.jpg";
-      if (!File.Exists(path))
-      {
-        File.Create(path);
-      }
-      else
-      {
-
-        Image image2 = Image.FromFile(Path.GetDirectoryName(
-            Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())) +
-            "\\Resources\\ProfilePicture.jpg");
-        image2.Dispose();
-      }
-
-      ContactsList = new ObservableCollection<UserModel>();
-
-      ContactsList.Add(new UserModel { Email = "email", FirstName = "Rodica", LastName = "Mihaela" });
-      ContactsList.Add(new UserModel { Email = "email2", FirstName = "Rodica", LastName = "Vasilescu" });
-      ContactsList.Add(new UserModel { Email = "email3", FirstName = "Random", LastName = "Name" });
-
-      MessagesList = new ObservableCollection<MessageModel>();
-      MessagesList.Add(new MessageModel { Content = "My first Message", HorizontalAlignment = HorizontalAlignment.Left });
-      MessagesList.Add(new MessageModel { Content = "My second Message", HorizontalAlignment = HorizontalAlignment.Right });
-      MessagesList.Add(new MessageModel { HorizontalAlignment = HorizontalAlignment.Left, Content = "My third Message.WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM" });
-      MessagesList.Add(new MessageModel { Content = "My last Message", HorizontalAlignment = HorizontalAlignment.Right });
     }
 
     private void SendCommandExecute()
@@ -224,11 +207,9 @@ namespace ChatApp.ViewModel
     #endregion
 
     #region Private Methods
+
     private void LogoutCommandExecute()
     {
-      //DialogResult dialogResult = MessageBox.Show("Are you sure you want to log out?", "Log out", MessageBoxButtons.YesNo);
-      //if (dialogResult == DialogResult.Yes)
-      //{
       var loginViewModel = new LoginViewModel(window);
       WindowManager.ChangeWindowContent(window, loginViewModel, Resources.LoginWindowTitle, Resources.LoginControlPath);
       window.VerticalContentAlignment = VerticalAlignment.Top;
@@ -239,9 +220,8 @@ namespace ChatApp.ViewModel
           win.Close();
         }
       }
-      //}
-
     }
+
     #endregion
 
     #region INotifyPropertyChanged
