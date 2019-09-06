fe8721fff13c2e654e547dfe1c02f8d2e6ea8a7e
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2019-09-06 14:32:21 +0300

    add regions to RelayCommand

--- ChatApp/ChatApp/Command/RelayCommand.cs
+++ ChatApp/ChatApp/Command/RelayCommand.cs
@@ -5,27 +5,42 @@ namespace ChatApp.Command
 {
   public class RelayCommand : ICommand
   {
+    #region Private Properties
+
+    private readonly Action _action;
+    private Func<bool> _canExecute;
+
+    #endregion
+
+    #region Constructor
+
+    public RelayCommand(Action action, Func<bool> canExecute = null)
+    {
+      _action = action;
+      _canExecute = canExecute;
+    }
+
+    #endregion
+
+    #region Public Methods
+
     public event EventHandler CanExecuteChanged
     {
       add { CommandManager.RequerySuggested += value; }
       remove { CommandManager.RequerySuggested -= value; }
     }
 
-    private readonly Action _action;
-
-    private Func<bool> _canExecute;
-    public RelayCommand(Action action, Func<bool> canExecute = null)
-    {
-      _action = action;
-      _canExecute = canExecute;
-    }
     public bool CanExecute(object parameter)
     {
       return _canExecute == null ? true : _canExecute.Invoke();
     }
+
     public void Execute(object parameter)
     {
       _action.Invoke();
     }
+
+    #endregion
+
   }
 }
